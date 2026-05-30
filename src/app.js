7cf0ccfb0f5ea1ec33a06def730c358c67629ba7
Sachin Bind <sachinbind7977@gmail.com>
2026-05-30 15:12:53 +0530

    install cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ const cookieParser = require('cookie-parser')
 const orderRouter = require('./router/order.router')
 const authRouter = require("./router/auth.router")
 const errorMiddleware = require("./middleware/errorMiddleware")
+const cors = require('cors')
 
 app.use(cookieParser())
 app.use(express.json())
-
+app,use(cors({
+    origin:'https://ganesh-power-laundry-frontend.vercel.app/'
+}))
 
 app.use('/api/order',orderRouter);
 app.use('/api/auth',authRouter)
